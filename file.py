82ee0a2638f1cff8c933b7613701479a82523e5a
john <jbichene@gmail.com>
2023-06-09 13:05:04 +0100

    test

--- file.py
+++ file.py
@@ -1,58 +1,53 @@
+import json
+
+
 def query(Brand  , model):
     the_cars = cars[Brand][model]
     for the_car in the_cars:
         print(the_car)
-    
-cars = {
-    
-    "Toyota":{
-        
-        
-        "camery":[
-            
-            {
-                "2020":{
-                    "Engine-type": "v6"
-                    , "tank":6 ,
-                    "electric": True
-                }
-            } ,
-            
-            {
-                "2021":{
-                    "Engine-type": "v8"
-                    , "tank":10,
-                    "electric": False
-                }
-            }
-            
-            
-        ] ,
-        "corrola":[
-            {
-                "2020":{
-                    "Engine-type": "v12"
-                    , "tank":5 ,
-                    "electric": True
-                }
-            }  
-            ]
-        
-    
-    } ,
-    "kia":{
-       "rio": [
-           {
-                "2019":{
-                    "Engine-type": None
-                    , "tank":5 ,
-                    "electric": True
-                }
-            } 
-           
-       ] 
-    }
-    
-}
 
-query("Toyota" , "camery")
+def save_db(data):
+    
+    with open("test.json" , "w+") as open_file:
+        json.dump(data , open_file)
+        
+def new_model_db(car_brand ,car_model ):
+    vehicles = cars[car_brand][car_model] 
+    vehicles.append(car_data)
+    cars[car_brand][car_model] = vehicles
+
+
+
+def add_db(car_brand ,car_model ):
+    vehicles = cars[car_brand] 
+    pint()
+    vehicles.update(car_model)
+    cars[car_brand] = vehicles
+
+
+def load_db():
+    with open("test.json", "r+") as file_open:
+        cars = json.load(file_open) 
+        
+    return cars
+
+cars = load_db()
+
+print(cars)   
+  
+add_db("Toyota" , {"tacoma":[{
+    "engine": "v8",
+    "tank":8,
+    "electric": False
+}]})
+save_db(cars)
+# print(cars)
+      
+    
+        
+   
+        
+    
+        
+
+
